3a5e2bb2f0dcfecb1989dca1a1d13491eb61c3d2
EggieMetal <edgardo.muniz@upr.edu>
2016-09-13 22:28:47 -0400

    same

--- Project 1/src/testerClasses/SequentialTester.java
+++ Project 1/src/testerClasses/SequentialTester.java
@@ -1,11 +1,27 @@
 package testerClasses;
 
+import java.util.Scanner;
+
+import Aproaches.SequentialFD;
+import experimentClasses.CreateFile;
+
 public class SequentialTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		SequentialFD<Integer> intList = new SequentialFD<Integer>();
+		SequentialFD<String> stringList = new SequentialFD<String>();
+		CreateFile file = new CreateFile();
+// Integers
+		file.openFile("int");
+		file.readFile("int");
+		file.closeFile();
 		
+// Strings		
+		file.openFile("string");
+		file.readFile("string");
+		file.closeFile();
 
 	}
 }
