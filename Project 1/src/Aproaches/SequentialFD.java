021cde828c55d1b580e78c48e8b517520acae747
EggieMetal <edgardo.muniz@upr.edu>
2016-09-14 14:14:19 -0400

    como hacer la lista usando Generics? Entender Metodo ya dado.
    trate de hacer la clase de sequentialFD con generics y me tranque. 
    ademas falta escribir los resultados.

--- Project 1/src/Aproaches/SequentialFD.java
+++ Project 1/src/Aproaches/SequentialFD.java
@@ -3,12 +3,16 @@ package Aproaches;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 import experimentClasses.FrequencyCounter;
 
 public class SequentialFD<E extends Comparable<E>> extends FrequencyCounter<E> 
 //TODO this class uses arrays
 {
+	private ArrayList<E> list = new ArrayList<>();
+	//	private Random rand = new Random();
+	//		rand.nextInt
 
 	public SequentialFD() {
 		super("Sequential");
@@ -37,4 +41,46 @@ public class SequentialFD<E extends Comparable<E>> extends FrequencyCounter<E>
 
 		return results;
 	}  // end of method computeFDList
+
+	private int SequentialCounter(E e)
+	{
+		//PRE: index and values are valid.
+
+		int counter = 0;
+		for(int i=0; i<list.size(); i++)
+		{
+			if(list.get(i).equals(e))
+			{
+				counter++;
+			}
+		}
+		return counter;
+	}
+
+	public void Sequencial()
+	{
+		ArrayList<E> counted = new ArrayList<>();
+
+		for(int i=0; i<list.size(); i++)
+		{
+			for (int j = 0; j < counted.size() - 1; j++)
+			{
+				if(list.get(i).equals(counted.get(j)))
+				{
+					//stop searching and continue to next key.
+					break;
+				}
+				else // i has not been counted yet.
+				{
+					
+					//TODO write the file with element j, counter SequentialCounter(list.get(i)), timeTaken
+					
+					SequentialCounter(list.get(i)); //TODO tengo las veces que se repiten, falta escribierlo en lista uno al lado del otro.
+					//podemos escribirlo en este metodo o en otro. o en otra clase.
+
+					counted.set(j+1, list.get(i)); //add element to counted list.
+				}
+			}
+		}
+	}
 }
--- Project 1/src/testerClasses/SequentialTester.java
+++ Project 1/src/testerClasses/SequentialTester.java
@@ -1,9 +1,110 @@
 package testerClasses;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+import java.util.Scanner;
+
 public class SequentialTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+class CreateFile 
+{
+
+	private Formatter x;
+	private Scanner s;
+
+	public void createIntFile()
+	{
+		try{
+			x = new Formatter("inputData/integerData.txt");
+		}
+		catch(Exception e){
+			System.out.println("you have an error.");
+		}
+	}
+
+	public void createStringFile()
+	{
+		try{
+			x = new Formatter("inputData/stringData.txt");
+		}
+		catch(Exception e){
+			System.out.println("you have an error.");
+		}
+	}
+
+	public void addIntData(int data, float time)
+	{
+		x.format("%s%s", data, time);
+	}
+
+	public void addStringData(String data, float time)
+	{
+		x.format("%s%s", data, time);
+	}
+
+	public void closeFile()
+	{
+		x.close();
+	}
+
+	public void openFile(String type)
+	{
+		try {
+			if(type.equals("int"))
+				s = new Scanner(new File("integerData.txt"));
+			else if( type.equals("string"))
+			{
+				s = new Scanner(new File("stringData.txt"));
+			}
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("could not find file");
+		}
+	}
+
+	public void readFile(String type)
+	{
+		if(type.equals("int"))
+		{
+			while(s.hasNext())
+			{
+				int a = s.nextInt();
+				float b = s.nextFloat();
+
+				System.out.printf("%s %s \n", a, b);
+			}
+		}
+		else if( type.equals("string"))
+		{
+			while(s.hasNext())
+			{
+				String a = s.next();
+				float b = s.nextFloat();
+
+				System.out.printf("%s %s \n", a, b);
+			}
+		}
+
+	}
+
 }
